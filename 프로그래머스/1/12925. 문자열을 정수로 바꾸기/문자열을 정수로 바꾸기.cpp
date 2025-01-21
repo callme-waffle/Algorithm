9d65dadc19caa889a661751b765f2061c405fa33
waffle <58645920+callme-waffle@users.noreply.github.com>
2025-01-22 00:31:31 +0900

    [level 1] Title: 문자열을 정수로 바꾸기, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/1/12925. 문자열을 정수로 바꾸기/문자열을 정수로 바꾸기.cpp
+++ 프로그래머스/1/12925. 문자열을 정수로 바꾸기/문자열을 정수로 바꾸기.cpp
@@ -4,5 +4,16 @@
 using namespace std;
 
 int solution(string s) {
-    return stoi(s);
+    int answer = 0;
+    bool is_minus = false;
+    for (int i = 0; i < s.size(); i++) {
+        if (s[i] == '+') continue;
+        if (s[i] == '-') {
+            is_minus = true;
+            continue;
+        }
+        answer *= 10;
+        answer += (s[i] - '0');
+    }
+    return answer * (is_minus ? -1 : 1);
 }
