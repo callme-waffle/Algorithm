20860f2de70c02303e1a4b8ae7d01693d8210c93
waffle <58645920+callme-waffle@users.noreply.github.com>
2025-01-22 10:13:25 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.01 ms, Memory: 4.13 MB -BaekjoonHub

---
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <cmath>
+
+using namespace std;
+
+int solution(int num) {
+    int cnt = 0;
+    long long calc = num;
+    
+    for (; cnt <= 500; cnt++) {
+        if (calc == 1) return cnt;
+        if (!(calc%2)) calc/=2;
+        else calc = (calc*3+1);
+    }
+    
+    return cnt > 500 ? -1 : cnt;
+}
