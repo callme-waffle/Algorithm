67ed0f31defec46634bb499f0cc2d5846e58fefb
waffle <58645920+callme-waffle@users.noreply.github.com>
2025-01-26 23:47:58 +0900

    [level 2] Title: 주식가격, Time: 28.14 ms, Memory: 26.8 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.cpp
+++ 프로그래머스/2/42584. 주식가격/주식가격.cpp
@@ -5,8 +5,8 @@
 using namespace std;
 
 vector<int> solution(vector<int> prices) {
-    vector<int> answer(prices.begin(), prices.end());
-    fill(answer.begin(), answer.end(), 0);
+    vector<int> answer(prices.size());
+
     for (int i = 0; i < prices.size(); i++) {
         for (int j = i+1; j < prices.size(); j++) {
             answer[i] = j-i;
