7ea9d90e9140efb4ca9ec9d3cf9094c3888779b1
waffle <58645920+callme-waffle@users.noreply.github.com>
2025-01-26 23:15:13 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 0.08 ms, Memory: 4.15 MB -BaekjoonHub

--- 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.cpp
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.cpp
@@ -1,33 +1,26 @@
 #include <string>
 #include <vector>
 #include <queue>
-#include <iostream>
 
 using namespace std;
 
-long long solution(int bridge_length, int weight, vector<int> truck_weights) {
-    long long t = 1ll; // 다음 차가 들어오는 시간
+int solution(int bridge_length, int weight, vector<int> truck_weights) {
+    int t = 1ll;
     queue<int> ot;
     
     auto s = truck_weights.begin();
     auto e = s;
     auto fin = truck_weights.end();
+    
     int w = 0, c = 0;
     while(s < fin) {
-        cout << "t: " << t << endl;
-        
-        while(w <= weight && c < bridge_length && e < fin) {
-            if((w + *e) > weight) break;
-            cout << "in: " << *e << " at " << t << " (~" << (t + bridge_length) << ")" << endl;
+        // 무게한도 or 길이한도 초과 전까지 계산
+        while((w + *e) <= weight && c < bridge_length && e < fin) {
             w += *e;
             e++;
             c++;
             ot.push((t++) + bridge_length);
         }
-        
-        cout << ot.size() << endl;
-
-        cout << "update t: " << ot.front() << endl;
         t = ot.front() > t ? ot.front() : t;
         ot.pop();
         w -= *s;
