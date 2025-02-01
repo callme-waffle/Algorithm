ccb2e8478dd051043affe2107cd9b52dabc4fd4d
waffle <58645920+callme-waffle@users.noreply.github.com>
2025-02-02 02:02:34 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 0.69 ms, Memory: 3.68 MB -BaekjoonHub

--- 프로그래머스/3/42627. 디스크 컨트롤러/README.md
+++ 프로그래머스/3/42627. 디스크 컨트롤러/README.md
@@ -1,10 +1,10 @@
 # [level 3] 디스크 컨트롤러 - 42627 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42627) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42627#) 
 
 ### 성능 요약
 
-메모리: 4.12 MB, 시간: 0.68 ms
+메모리: 3.68 MB, 시간: 0.69 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 02월 02일 02:00:17
+2025년 02월 02일 02:02:32
 
 ### 문제 설명
 
--- 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.cpp
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.cpp
@@ -3,8 +3,6 @@
 #include <queue>
 #include <algorithm>
 
-// #include <iostream>
-
 using namespace std;
 
 struct cmp {
@@ -35,21 +33,17 @@ int solution(vector<vector<int>> jobs) {
     // 가장 빠른 시작시간으로 이동하여 시작 (0초작업 없는경우 고려)
     time.push((*job)[0]);
     stq.push(vector<int>({(*job)[1], (*job)[0], id++})); // 소요시간, 요청시간, ID
-    // cout << "[PREADD]" << "== | Process ADD: " << id-1 << "| need: " << (*job)[1] << " | ==" << endl;
     job++;
     
     while(!time.empty()) {
         curr = time.top(); time.pop();
-        // cout << "[TIME_" << curr << "]" << endl;
         
         if (pet == curr) { // 프로세스가 종료되는 경우
-            // cout << "[TIME_" << curr << "]" << "== | Process END: " << prt << "~" << pet << " | ==" << endl;
             res += (pet - prt);
             pet = -1; prt = -1;
         }
         
         while(job < jobs.end() && (*job)[0] ==curr) { // 추가할 값이 있는경우 queue에 추가
-            // cout << "[TIME_" << curr << "]" << "== | Process ADD: " << id << "| need: " << (*job)[1] << " | ==" << endl;
             stq.push(vector<int>({(*job)[1], (*job)[0], id++})); // 소요시간, 요청시간, ID
             job++;
         }
@@ -58,13 +52,10 @@ int solution(vector<vector<int>> jobs) {
         if (stq.empty()) continue; // 검토할 시간은 있는데 실행스택만 비어있다면 다음 검토시간으로 이동
         
         if (pet > -1) continue; // 실행중인 프로세스가 있는 경우 더이상 진행하지 않음
-
-        // cout << "[TIME_" << curr << "]" << "== | Process Start: " << stq.top()[2] << " | requested: " << stq.top()[1] << " | duration: " << stq.top()[0] << " | ==" << endl;
         
         prt = stq.top()[1]; pet = curr + stq.top()[0]; stq.pop();
         time.push(pet);
-        
-        
+
     }
     
     return res/(jobs.size());
