925baf4075806a68effcc373ca9afccc8b73b94a
waffle <58645920+callme-waffle@users.noreply.github.com>
2025-02-03 13:34:22 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.02 ms, Memory: 4.02 MB -BaekjoonHub

--- 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.cpp
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.cpp
@@ -1,34 +1,18 @@
 #include <string>
 #include <vector>
-#include <unordered_map>
+#include <iostream>
 
 using namespace std;
 
-int solution(string s) {
-    unordered_map<string, pair<string, int>> a = {
-        {"ze", {"0", 4}},
-        {"on", {"1", 3}},
-        {"tw", {"2", 3}},
-        {"th", {"3", 5}},
-        {"fo", {"4", 4}},
-        {"fi", {"5", 4}},
-        {"si", {"6", 3}},
-        {"se", {"7", 5}},
-        {"ei", {"8", 5}},
-        {"ni", {"9", 4}}
-    };
-    string ans = "";
-    for (int i = 0; i < s.size(); i++) {
-        char v = s[i];
-        if ('0' <= v && v <= '9') {
-            ans += v;
-            continue;
-        }
+vector<string> numbers = { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
 
-        string key = s.substr(i, 2);
-        i += (a[key].second-1);
-        ans += a[key].first;
+int solution(string s) {
+    for (int i = 0; i < 10; i++) {
+        string number = numbers[i];
+        while(s.find(number) != string::npos) { // number가 계속 발견되는 동안
+            s.replace(s.find(number), number.size(), to_string(i));// 문자를 숫자로 대체
+        }
     }
-    
-    return stoi(ans);
+
+    return stoi(s);
 }
