f32958521b3c24e51742e28401326d00bfb862d0
waffle <58645920+callme-waffle@users.noreply.github.com>
2025-02-06 11:21:30 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 284 ms, Memory: 3980 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.cc
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.cc
@@ -1,43 +1,51 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-#include <unordered_map>
 
 using namespace std;
 
-int n, m, c, nc = 0;
-vector<int> nums;
-unordered_map<int, int> cnt;
+int N, M;
+vector<int> num;
 
-int bisect(int l, int r);
+int bisectQ1(int l, int r);
+int bisectQ2(int l, int r);
 
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(0); cout.tie(0);
 
-    cin >> n; int t;
-    nums.resize(n);
-    for (int i = 0; i < n; i++) {
-        cin >> t;
-        if (cnt[t] == 0) nums[nc++] = t;
-        cnt[t]++;
-    }
-    
-    sort(nums.begin(), nums.end());
+    cin >> N;
 
-    cin >> m;
-    for (int i = 0; i < m; i++) {
-        cin >> c;
-        cout << bisect(0, n) << '\n';
+    num.resize(N);
+    for (int i = 0; i < N; i++) cin >> num[i];
+    sort(num.begin(), num.end());
+
+    int cnM;
+    cin >> cnM;
+    for (int i = 0; i < cnM; i++) {
+        cin >> M;
+        int se_idx = bisectQ1(-1, N); // 작거나 같은지
+        int s_idx = bisectQ2(-1, N); // 작은지
+        cout << se_idx - s_idx << ' ';
     }
 
     return 0;
 }
 
-int bisect(int l, int r) {
-    if (r-l <= 1) return nums[l] == c ? cnt[c] : 0;
+int bisectQ1(int l, int r) {
+    // 중단조건
+    if (r-l <= 1) return l; // l/r 결정조건 확인필요
     
     int mid = (l+r)/2;
-    if (nums[mid] <= c) return bisect(mid, r);
-    return bisect(l, mid);
+    if (num[mid] <= M) return bisectQ1(mid, r);
+    return bisectQ1(l, mid);
+}
+
+int bisectQ2(int l, int r) {
+    // 중단조건
+    if (r-l <= 1) return l; // l/r 결정조건 확인필요
+    
+    int mid = (l+r)/2;
+    if (num[mid] < M) return bisectQ2(mid, r);
+    return bisectQ2(l, mid);
 }
