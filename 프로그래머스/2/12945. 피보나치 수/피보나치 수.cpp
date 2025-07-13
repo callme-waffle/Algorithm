bc4ca66f791bb7dc3533867cdc702dfa7a7f9734
Kim Hwagyun <58645920+callme-waffle@users.noreply.github.com>
2025-07-13 21:39:09 +0900

    [level 2] Title: 피보나치 수, Time: 2.98 ms, Memory: 6.65 MB -BaekjoonHub

--- 프로그래머스/2/12945. 피보나치 수/피보나치 수.cpp
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.cpp
@@ -1,21 +1,20 @@
+#include <iostream>
 #include <string>
 #include <vector>
 
 using namespace std;
 
-vector<int> sp = {-1};
+vector<int> sp;
+
+int f(int n);
+
 int solution(int n) {
-    if (sp[0] == -1) {
-        vector<int> a(n+1, -1);
-        sp = a;
-        sp[0] = 0;
-        sp[1] = 1;
-    }
+    sp = vector<int>(n+1, -1);
+    sp[0] = 0; sp[1] = 1;
     
-    if (sp[n] > -1) return sp[n];
-    
-    int res = (solution(n-1) + solution(n-2)) % 1234567;
-    sp[n] = res;
-    
-    return res;
+    return f(n);
+}
+int f(int n) {
+    if (sp[n] == -1) sp[n] = (f(n-1) + f(n-2))%1234567;
+    return sp[n];
 }
