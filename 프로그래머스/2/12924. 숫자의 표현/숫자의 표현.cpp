79e187b1c6550c2cec4c8ec04579cfe17f0a7de5
Kim Hwagyun <58645920+callme-waffle@users.noreply.github.com>
2025-07-13 21:23:26 +0900

    [level 2] Title: 숫자의 표현, Time: 0.08 ms, Memory: 3.88 MB -BaekjoonHub

--- 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.cpp
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.cpp
@@ -1,20 +1,23 @@
 #include <string>
+#include <iostream>
 #include <vector>
 
 using namespace std;
 
 int solution(int n) {
-    int sn = 1, cnt = 0;
-    while(sn <= n) {
-        int sum = 0;
-        for (int an = sn; sum <= n; an++) {
-            sum += an;
-            if (sum == n) {
-                cnt++;
-                break;
-            }
+    int s = 1, e = 1;
+    int cursum = 1;
+    int cnt = 0;
+    while(s <= n && e <= n) {
+        if (cursum < n) cursum += (++e);
+        else if (cursum > n) cursum -= (s++);
+        
+        if (cursum == n) {
+            cout << s << " " << e << endl;
+            cnt++; 
+            e++; cursum += e;
+            continue;
         }
-        sn++;
     }
     
     return cnt;
