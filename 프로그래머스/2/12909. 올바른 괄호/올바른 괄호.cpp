423a4883eff7c94d0f8157517dae7c634ad39562
Kim Hwagyun <58645920+callme-waffle@users.noreply.github.com>
2025-07-13 21:09:53 +0900

    [level 2] Title: 올바른 괄호, Time: 0.39 ms, Memory: 3.83 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.cpp
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.cpp
@@ -3,18 +3,14 @@
 
 using namespace std;
 
-bool solution(string s) {
+bool solution(string s)
+{
     int cnt = 0;
-    for (char a: s) {
-        switch(a) {
-            case '(':
-                cnt++;
-                break;
-            case ')':
-                cnt--;
-                if (cnt < 0) return false;
-                break;
-        }
+    for (char c: s) {
+        if (c == '(') cnt++;
+        else if(cnt == 0) return false;
+        else cnt--;
     }
+    
     return cnt == 0;
 }
